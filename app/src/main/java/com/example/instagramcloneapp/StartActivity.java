8eeadc930da442603a5833cbc4744857d966d943
ay942 <ay942115@gmail.com>
2022-12-20 15:37:15 +0530

    v

--- app/src/main/java/com/example/instagramcloneapp/StartActivity.java
+++ app/src/main/java/com/example/instagramcloneapp/StartActivity.java
@@ -1,14 +1,50 @@
 package com.example.instagramcloneapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class StartActivity extends AppCompatActivity {
-
+    private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        imageView = findViewById(R.id.imageView);
+        imageView.animate().alpha(0f).setDuration(10);
+
+        TranslateAnimation animation = new TranslateAnimation(0 , 0 , 0 , -1500);
+        animation.setDuration(1000);
+        animation.setFillAfter(false);
+        animation.setAnimationListener(new MyAnimationListener());
+
+        imageView.setAnimation(animation);
+
+    }
+
+    private class MyAnimationListener implements Animation.AnimationListener {
+        @Override
+        public void onAnimationStart(Animation animation) {
+
+        }
+
+        @Override
+        public void onAnimationEnd(Animation animation) {
+            imageView.clearAnimation();
+            imageView.setVisibility(View.INVISIBLE);
+            imageView.animate().alpha(1f).setDuration(1000);
+
+        }
+
+        @Override
+        public void onAnimationRepeat(Animation animation) {
+
+        }
     }
 }
