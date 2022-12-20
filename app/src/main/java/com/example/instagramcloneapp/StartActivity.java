3509f8cfa6a7fb2bf7079995f4e13201357e5e4f
Ranjan-786 <ranjan.kumar_cs18@gla.ac.in>
2022-12-20 15:42:19 +0530

    ranjan update

--- app/src/main/java/com/example/instagramcloneapp/StartActivity.java
+++ app/src/main/java/com/example/instagramcloneapp/StartActivity.java
@@ -1,14 +1,47 @@
 package com.example.instagramcloneapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class StartActivity extends AppCompatActivity {
 
+    Button button2,button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        button2 = findViewById(R.id.button2);
+        button=findViewById(R.id.button);
+
+        userLogin();
+        userRegister();
+
+    }
+
+    private void userRegister()
+    {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+
+            }
+        });
+    }
+
+    private void userLogin()
+    {
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                startActivity(new Intent(StartActivity.this,MainActivity.class));
+            }
+        });
     }
 }
